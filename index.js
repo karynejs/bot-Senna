35a58e3b8ab78ef7782cb7a6a655a339b07c6be7
Karyne <Karyneman05@gmail.com>
2026-05-29 20:28:13 -0300

    corrigir admin

--- index.js
+++ index.js
@@ -40,5 +40,52 @@ if (
 ) {
     await message.react('👑');
 }
+if (message.body === '!fechar') {
+    const chat = await message.getChat();
+
+    if (chat.isGroup) {
+        await chat.setMessagesAdminsOnly(true);
+        message.reply('🔒 Grupo fechado!');
+    }
+}
+
+if (message.body === '!abrir') {
+    const chat = await message.getChat();
+
+    if (chat.isGroup) {
+        await chat.setMessagesAdminsOnly(false);
+        message.reply('🔓 Grupo aberto!');
+    }
+}
+if (message.body.startsWith('!ban')) {
+
+    const chat = await message.getChat();
+
+    if (!chat.isGroup) {
+        return message.reply('❌ Este comando só funciona em grupos.');
+    }
+
+    const author = message.author || message.from;
+
+    const admins = chat.participants.filter(p => p.isAdmin || p.isSuperAdmin);
+
+    const isAdmin = admins.some(admin => author.includes(admin.id.user));
+
+    console.log('AUTHOT:', author);
+    console.log('ADMINS:', admins.map(a => a.id._serialized));
+    if (!isAdmin) {
+        return message.reply('❌ Apenas administradores podem usar este comando, porra.');
+    }
+
+    const mentionedUsers = message.mentionedIds;
+
+    if (!mentionedUsers.length) {
+        return message.reply('❌ Marque alguém para banir, sua bura.');
+    }
+
+    await chat.removeParticipants(mentionedUsers);
+
+    message.reply('🚫 Membro removido, rs.');
+}
 });
 client.initialize();
